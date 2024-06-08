ca999ba7ba5575e447a0c4dd6e6618f8bebe9801
Md Saif Ali Khan <itlt2307@glbitm.ac.in>
2024-06-09 00:39:53 +0530

    Tic Tac Game

---
+++ index.js
@@ -0,0 +1,83 @@
+
+
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let gameActive = true;
+const statusDisplay = document.getElementById('status');
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameBoard[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameBoard[winCondition[0]];
+        let b = gameBoard[winCondition[1]];
+        let c = gameBoard[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = `Player ${currentPlayer} has won!`;
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameBoard.includes('');
+    if (roundDraw) {
+        statusDisplay.innerHTML = 'Game ended in a draw!';
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+
+    if (gameBoard[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+    currentPlayer = 'X';
+    statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = '');
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.getElementById('resetButton').addEventListener('click', handleRestartGame);
+statusDisplay.innerHTML = `It's ${currentPlayer}'s turn`;
